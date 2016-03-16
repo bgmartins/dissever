5beed457a7e11cc48e723d87d8cedc3020ab6939
Bruno Martins <bgmartins@users.noreply.github.com>
2016-03-16 18:03:09 +0000

    Update dissever.R

--- R/dissever.R
+++ R/dissever.R
@@ -56,7 +56,7 @@ utils::globalVariables(c(
 }
 
 .create_lut_fine <- function(coarse, fine) {
-  extract(coarse, coordinates(fine), na.rm=FALSE)
+  extract(coarse, coordinates(fine), na.rm=TRUE)
 }
 
 .default_control_init <- caret::trainControl(
