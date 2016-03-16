858d782daeae86254787caa6c51e714abe2bca97
Bruno Martins <bgmartins@users.noreply.github.com>
2016-03-16 18:14:54 +0000

    Update dissever.R

--- R/dissever.R
+++ R/dissever.R
@@ -262,7 +262,6 @@ utils::globalVariables(c(
   # Convert coarse data to data.frame
   coarse_df <- .as_data_frame_factors(coarse, xy = TRUE)
   coarse_df$cell <- 1:nrow(coarse_df) # integer
-  # coarse_df <- na.exclude(coarse_df)
 
   # Convert fine data to data.frame
   fine_df <- .as_data_frame_factors(fine, xy = TRUE)
@@ -270,15 +269,12 @@ utils::globalVariables(c(
   fine_df[['cell']] <- as.integer(.create_lut_fine(ids_coarse, fine))
   fine_df <- na.exclude(fine_df)
   
-  print( coarse_df$cell )
-  print( fine_df$cell )
-  print( nm_coarse )
-  
   # Resampled national model onto fine grid
   fine_df <- cbind(
     fine_df,
     .join_interpol(coarse_df = coarse_df[, c('cell', nm_coarse)], fine_df = fine_df, attr = nm_coarse, by = 'cell')
   )
+  coarse_df <- na.exclude(coarse_df)
   fine_df <- na.exclude(fine_df)
   
   if (is.null(p)) {
