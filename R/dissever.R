00265255e2eb440208b0020139d8de5e6b886b4f
Bruno Martins <bgmartins@users.noreply.github.com>
2016-03-28 17:23:32 +0100

    Update dissever.R

--- R/dissever.R
+++ R/dissever.R
@@ -276,6 +276,7 @@ utils::globalVariables(c(
   } else {
     coarse_df <- .as_data_frame_factors(coarse, xy = TRUE)
     coarse_df$cell <- .as_data_frame_factors(ids_coarse, xy = TRUE)$cell
+    coarse_df$cell[is.na(coarse_df$cell)] <- max( coarse_df$cell ) + 1
   } 
 
   print( ncell(coarse) )
