992439c77f0d68b09befdfed1d95d1f5368ef53d
Bruno Martins <bgmartins@users.noreply.github.com>
2016-03-28 17:29:03 +0100

    Update dissever.R

--- R/dissever.R
+++ R/dissever.R
@@ -276,15 +276,10 @@ utils::globalVariables(c(
   } else {
     coarse_df <- .as_data_frame_factors(coarse, xy = TRUE)
     coarse_df$cell <- .as_data_frame_factors(ids_coarse, xy = TRUE)[['cell']]
-    coarse_df$cell <- replace(coarse_df$cell, which(is.na(coarse_df$cell)), -1)
   } 
 
-  print( ncell(coarse) )
-  print( ncell(ids_coarse) )
-  print( nrow(coarse_df) )
-  print( nrow( .as_data_frame_factors(coarse, xy = TRUE)) )
-  print( coarse_df$cell )
-
+  coarse_df <- na.exclude(coarse_df)
+  
   # Convert fine data to data.frame
   fine_df <- .as_data_frame_factors(fine, xy = TRUE)
   # Add coarse cell ID to fine data.frame
