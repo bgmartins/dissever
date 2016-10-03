f98eaf1454560d6580346800e2560d0b0e1da82b
Bruno Martins <bgmartins@users.noreply.github.com>
2016-10-03 10:07:24 +0100

    Update dissever.R

--- R/dissever.R
+++ R/dissever.R
@@ -290,8 +290,8 @@ utils::globalVariables(c( "cell", "diss", ".", "matches", "i"))
       if (verbose) message('| -- updating model')
       fit <- .update_model( vars = fine_df[id_spl, nm_covariates], y = diss_result[id_spl, 'diss', drop = TRUE], method = method, control = train_control_iter, tune_grid = best_params, data_type = data_type )
       if (verbose) message('| -- updating predictions')
-      if ( method == 'lme' ) fine_df[['dummy']] <- rep.int(1,nrow(fine_df))
-      diss_result$diss <- .predict_map(fit, fine_df, split = split_cores, boot = NULL, data_type=data_type)
+      if ( method == 'lme' ) diss_result$diss <- .predict_map(fit,data.frame(fine_df,dummy=rep.int(1,nrow(fine_df))), split = split_cores, boot = NULL, data_type=data_type)
+      else diss_result$diss <- .predict_map(fit, fine_df, split = split_cores, boot = NULL, data_type=data_type)
     } else {
       varaux = fine_df[id_spl, nm_covariates]
       varr = diss_result[id_spl, 'diss', drop = TRUE]
@@ -358,8 +358,8 @@ utils::globalVariables(c( "cell", "diss", ".", "matches", "i"))
   }
   if (verbose) message('Retaining model fitted at iteration ', best_iteration)
   if( method == 'gwr' ) { map <- fit$SDF$prediction } else {
-    if ( method == 'lme' ) fine_df[['dummy']] <- rep.int(1,nrow(fine_df))
-    map <- .predict_map(best_model, fine_df, split = split_cores, boot = boot, level = level, data_type=data_type)
+    if ( method == 'lme' ) map <- .predict_map(best_model,data.frame(fine_df,dummy=rep.int(1,nrow(fine_df))), split = split_cores, boot = boot, level = level, data_type=data_type)
+    else map <- .predict_map(best_model, fine_df, split = split_cores, boot = boot, level = level, data_type=data_type)
   }
   if (data_type == 'count') { map[map < 0.0] <- 0 }
   map <- rasterFromXYZ( data.frame( diss_result[, c('x', 'y')], diss = map ), res = res(fine), crs = projection(fine) )
