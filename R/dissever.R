1c2add585ec7d6acb0b725411036de5138141f80
Bruno Martins <bgmartins@users.noreply.github.com>
2016-10-03 07:16:47 +0100

    Update dissever.R

--- R/dissever.R
+++ R/dissever.R
@@ -219,7 +219,7 @@ utils::globalVariables(c( "cell", "diss", ".", "matches", "i"))
   # Resampled model onto fine grid
   fine_df <- cbind(
     fine_df[, c('x', 'y', 'cell', 'cell2', 'cell3' , nm_covariates)],
-    .join_interpol(coarse_df = coarse_df[, c('cell', 'cell2', nm_coarse)], fine_df = fine_df, attr = c('cell3', nm_coarse), by = 'cell2')
+    .join_interpol(coarse_df = coarse_df[, c('cell', 'cell2', nm_coarse)], fine_df = fine_df, attr = nm_coarse, by = 'cell2')
   )
   coarse_df <- na.exclude(coarse_df)
   fine_df <- na.exclude(fine_df)
@@ -235,6 +235,7 @@ utils::globalVariables(c( "cell", "diss", ".", "matches", "i"))
   print (id_spl)
   print (id_spl_aux)
   print (fine_df$cell3 )
+                             
   id_spl <- sample(1:nrow(fine_df), size = n_spl) # sample random grid cells
   if (verbose) message('Selecting best model parameters')
   y_aux = fine_df[id_spl, nm_coarse, drop = TRUE]  
