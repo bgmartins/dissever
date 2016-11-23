73a42f3bf1d5f9aab49381f927ed7805e0d3314a
Bruno Martins <bgmartins@users.noreply.github.com>
2016-11-23 14:13:59 +0000

    Update dissever.R

--- R/dissever.R
+++ R/dissever.R
@@ -177,9 +177,9 @@ utils::globalVariables(c( "cell", "diss", ".", "matches", "i"))
   repeat {
     old.x <- x
     mval <- mean(x)
-    s1d <- function(s) unclass(filter(s,c(0.5,0,0.5)))
+#    s1d <- function(s) unclass(filter(s,c(0.5,0,0.5)))
     pad <- rbind(mval,cbind(mval,x,mval),mval)
-    pad <- (t(apply(pad,1,s1d)) + apply(pad,2,s1d))/2
+#    pad <- (t(apply(pad,1,s1d)) + apply(pad,2,s1d))/2
     sm <- (pad[2:(nrow(x)+1),2:(ncol(x)+1)])
     x <- x*r + (1-r)*sm
     for (item in zone.list) {
